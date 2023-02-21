6616453c5e70369e82df75e3a07b4d567126ce0e
Victor Caballero <vcaballero_c@hotmail.com>
2023-02-21 17:52:20 -0500

    gymFix  GO

---
+++ wp-content/themes/gymFix/functions.php
@@ -0,0 +1,23 @@
+
+
+<?php
+
+function gymfitness_menus(){
+    /* Se pueen agregar mas menus solo separar con  comas (,) */ 
+    register_nav_menus( array(
+        'menu_principal' => __('Menu Principal', 'gymfitness' )
+
+    ) );
+
+}
+ 
+/* add_action:  agrega codigo
+   add_filter: modifica la información
+*/
+
+/*  INIT se le conoce como un hook  ( este arranca el wp) */
+
+add_action('init', 'gymfitness_menus');
+
+
+
---
+++ wp-content/themes/gymFix/index.php
@@ -0,0 +1,52 @@
+
+
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+
+<header class="header">
+    <div class="contenedor barra-navegacion">
+        <div class="logo">
+               <!-- get_template_directory_uri()  FUNCION QUE MUESTRA LA RUTA DEL  TEMPLATE  -->
+            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
+
+        </div>
+
+        <nav>
+         
+            <a href="./nosotros">Nosotros</a>
+            <a href="./contacto">Contacto</a>
+            <a href="./blog">Blog</a>
+
+        </nav>
+
+    </div>
+
+
+</header>
+
+<main>
+<?php 
+
+    while( have_posts() ) : the_post();
+
+        the_title();
+
+        the_content();
+
+    endwhile;
+
+
+?></main>    
+</body>
+</html>
