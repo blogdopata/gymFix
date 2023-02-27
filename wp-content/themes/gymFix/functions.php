545831f16e9c8e581e08dad4c46dbb5b9c5fa8ea
Victor Caballero <vcaballero_c@hotmail.com>
2023-02-27 17:57:45 -0500

    agregando recursos

--- wp-content/themes/gymFix/functions.php
+++ wp-content/themes/gymFix/functions.php
@@ -21,4 +21,14 @@ function gymfitness_menus(){
 add_action('init', 'gymfitness_menus');
 
 
+/*  Para agregar una hoja de EStilos */
 
+
+function  gymfitness_scripts_styles(){
+
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0' ) ;
+
+
+}
+
+add_action('wp_enqueue_scripts', 'gymfitness_scripts_styles');
--- wp-content/themes/gymFix/index.php
+++ wp-content/themes/gymFix/index.php
@@ -8,6 +8,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- wp_head()  para cargar tus recursos -->
+    <?php
+        wp_head();
+    ?>
     <title>Document</title>
 </head>
 
