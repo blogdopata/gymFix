a4625d9f54eddf750db6edd1dc936a07bd8dc3e3
Vict0r Caballero <vcaballero_c@hotmail.com>
2023-03-11 02:10:42 -0500

    segmentando partes del tema

---
+++ wp-content/themes/gymFix/header.php
@@ -0,0 +1,46 @@
+
+
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- wp_head()  para cargar tus recursos -->
+    <?php
+        wp_head();
+    ?>
+    <title>Document</title>
+</head>
+
+<body>
+
+<header class="header">
+    <div class="contenedor barra-navegacion">
+        <div class="logo">
+               <!-- get_template_directory_uri()  FUNCION QUE MUESTRA LA RUTA DEL  TEMPLATE  -->
+            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
+
+        </div>
+
+        <?php 
+
+        /* con el themre_location indico que menu quiero renderizar en pantalla "*/
+            $args = array(
+                'theme_location' => 'menu_principal',
+                'container' => 'nav',
+                'container_class' => 'menu-principal'
+
+            );  
+
+            wp_nav_menu($args);
+
+
+        ?>
+
+    </div>
+
+
+</header>
--- wp-content/themes/gymFix/page.php
+++ wp-content/themes/gymFix/page.php
@@ -1,60 +1,18 @@
+<?php get_header();
+
+?>
 
 
 
 
 
 
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- wp_head()  para cargar tus recursos -->
-    <?php
-        wp_head();
-    ?>
-    <title>Document</title>
-</head>
-
-
-<body>
-
-<header class="header">
-    <div class="contenedor barra-navegacion">
-        <div class="logo">
-               <!-- get_template_directory_uri()  FUNCION QUE MUESTRA LA RUTA DEL  TEMPLATE  -->
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
-
-        </div>
-
-        <?php 
-
-        /* con el themre_location indico que menu quiero renderizar en pantalla "*/
-            $args = array(
-                'theme_location' => 'menu_principal',
-                'container' => 'nav',
-                'container_class' => 'menu-principal'
-
-            );  
-
-            wp_nav_menu($args);
-
-
-        ?>
-
-    </div>
-
-
-</header>
-
-    <main>
+    <main class="contenedor seccion">
             <?php 
 
                 while( have_posts() ) : the_post();
 
-                    the_title();
+                    the_title('<h1 class="text-center text-primary">' , '</h1>');
 
                     the_content();
 
