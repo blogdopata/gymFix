1d36add1cac67ca8e9b78542073e7968c72316db
Victor Caballero <vcaballero_c@hotmail.com>
2023-03-17 18:52:20 -0500

    menu footer

--- wp-content/themes/gymFix/footer.php
+++ wp-content/themes/gymFix/footer.php
@@ -5,7 +5,21 @@
 <footer class='footer contenedor'>
     <hr>
 
-    <h1>Desde Footer</h1>
+    <?php 
+
+    /* con el themre_location indico que menu quiero renderizar en pantalla "*/
+        $args = array(
+            'theme_location' => 'menu_principal',
+            'container' => 'nav',
+            'container_class' => 'menu-principal'
+
+        );  
+
+        wp_nav_menu($args);
+
+
+?>
+
 
 </footer>
 
