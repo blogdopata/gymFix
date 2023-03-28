70aabbf8d6824646c2c32e341167b0d73b9f04a5
Victor Caballero <vcaballero_c@hotmail.com>
2023-03-28 17:44:55 -0500

    creando plugin

---
+++ wp-content/plugins/gymfix/gymfitness_post_types.php
@@ -0,0 +1,10 @@
+<?php
+/*
+    Plugin Name: Gym Fitness - Post Types
+    Plugin URI: http://twitter.com/vict0rcaballero
+    Description: Añade Post Types al sitio Gym Fitness
+    Version: 1.0.0
+    Author: Victor Caballero Cabrera
+    Author URI: http://twitter.com/vict0rcaballero
+    Text Domain: gymfix
+*/
